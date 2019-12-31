7ce575854d44276766c9a32156cfbeea6561b48e
Yevgeniy Tkachenko <13heavyweather@gmail.com>
2019-12-30 22:32:35 -0500

    Added service discovery.

---
+++ const.js
@@ -0,0 +1,8 @@
+module.exports = {
+    NANOLEAF_AURORA_TARGET: 'nanoleaf_aurora:light',
+    M_SEARCH: 'm-search',
+    SSDP_DEFAULT_IP: '239.255.255.250',
+    ANY_IP: '0.0.0.0',
+    SSDP_DEFAULT_PORT: 1900,
+    SSDP_SOURCE_PORT: 1901
+}
--- index.js
+++ index.js
@@ -1,1 +1,55 @@
+var request = require('request');
+var dgram = require('dgram');
+var c = require('./const');
+
 console.log("Hello World!");
+
+var NanoleafApi = module.exports = function (options) {
+    var self = this;
+
+    this.testRequest = () => {
+        request('http://www.google.com', function (error, response, body) {
+            console.log('error:', error); // Print the error if one occurred
+            console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+            console.log('body:', body); // Print the HTML for the Google homepage.
+        });
+    }
+}
+
+//let test = new NanoleafApi();
+//test.testRequest();
+
+function broadcastSsdp(socket) {
+    var query = Buffer.from(
+        'M-SEARCH * HTTP/1.1\r\n'
+    + `HOST: ${c.SSDP_DEFAULT_IP}:${c.SSDP_DEFAULT_PORT}\r\n`
+    + 'MAN: "ssdp:discover"\r\n'
+    + 'MX: 1\r\n'
+    + `ST: ${c.NANOLEAF_AURORA_TARGET}\r\n\r\n`);
+  
+    socket.send(query, 0, query.length, c.SSDP_DEFAULT_PORT, c.SSDP_DEFAULT_IP);
+  }
+
+function createSocket() {
+    var socket = dgram.createSocket('udp4');
+
+    socket.on('listening', function () {
+      console.log('socket ready...');
+      broadcastSsdp(socket);
+    });
+  
+    socket.on('message', function (chunk, info) {  
+      console.log('[incoming] UDP message');
+      console.log(info);
+      console.log('MESSAGE', chunk.toString());
+    });
+  
+    console.log('binding to', c.ANY_IP + ':' + c.SSDP_SOURCE_PORT);
+    socket.bind(c.SSDP_SOURCE_PORT, c.ANY_IP);
+
+    setTimeout(() => {
+        socket.close();
+    }, 3000)
+  }
+
+// createSocket();
