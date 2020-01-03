655e062b4e8066968c2cf96ad00f2c8b3f10401d
Vadym Harkusha <vadgig@gmail.com>
2020-01-02 21:28:21 -0500

    Enabled ES6 modules

--- const.js
+++ const.js
@@ -1,9 +1,11 @@
-module.exports = {
-    NANOLEAF_AURORA_TARGET: 'nanoleaf_aurora:light',
-    M_SEARCH: 'm-search',
-    SSDP_DEFAULT_IP: '239.255.255.250',
-    ANY_IP: '0.0.0.0',
-    SSDP_DEFAULT_PORT: 1900,
-    SSDP_SOURCE_PORT: 1901,
-    NANOLEAF_PORT: 16021
-}
+const c = {
+  NANOLEAF_AURORA_TARGET: 'nanoleaf_aurora:light',
+  M_SEARCH: 'm-search',
+  SSDP_DEFAULT_IP: '239.255.255.250',
+  ANY_IP: '0.0.0.0',
+  SSDP_DEFAULT_PORT: 1900,
+  SSDP_SOURCE_PORT: 1901,
+  NANOLEAF_PORT: 16021
+};
+
+export default c;
--- index.js
+++ index.js
@@ -1,53 +1,55 @@
-var c = require('./const');
-let NanoleafHttpClient = require('./nanoleaf-http-client');
-var dgram = require('dgram');
+import c from './const.js';
+import NanoleafHttpClient from './src/nanoleaf-http-client.js';
+import dgram from 'dgram';
 
-console.log("Hello World!");
-
-var client = new NanoleafHttpClient('192.168.0.10', 'qEQ8ZLcPuOVesarDXIW6eGQQd1Hhn1d9');
+var client = new NanoleafHttpClient(
+  '192.168.0.10',
+  'qEQ8ZLcPuOVesarDXIW6eGQQd1Hhn1d9'
+);
 
 function getState() {
   client.getRequest('state/on');
 }
 
 function turnOn() {
-  client.putRequest('state', JSON.stringify({on: { value: true } }, null, 2));
+  client.putRequest('state', JSON.stringify({ on: { value: true } }, null, 2));
 }
 
 //getState();
 //turnOn();
 
 function broadcastSsdp(socket) {
-    var query = Buffer.from(
-        'M-SEARCH * HTTP/1.1\r\n'
-    + `HOST: ${c.SSDP_DEFAULT_IP}:${c.SSDP_DEFAULT_PORT}\r\n`
-    + 'MAN: "ssdp:discover"\r\n'
-    + 'MX: 1\r\n'
-    + `ST: ${c.NANOLEAF_AURORA_TARGET}\r\n\r\n`);
-  
-    socket.send(query, 0, query.length, c.SSDP_DEFAULT_PORT, c.SSDP_DEFAULT_IP);
-  }
+  var query = Buffer.from(
+    'M-SEARCH * HTTP/1.1\r\n' +
+      `HOST: ${c.SSDP_DEFAULT_IP}:${c.SSDP_DEFAULT_PORT}\r\n` +
+      'MAN: "ssdp:discover"\r\n' +
+      'MX: 1\r\n' +
+      `ST: ${c.NANOLEAF_AURORA_TARGET}\r\n\r\n`
+  );
+
+  socket.send(query, 0, query.length, c.SSDP_DEFAULT_PORT, c.SSDP_DEFAULT_IP);
+}
 
 function createSocket() {
-    var socket = dgram.createSocket('udp4');
+  var socket = dgram.createSocket('udp4');
 
-    socket.on('listening', function () {
-      console.log('socket ready...');
-      broadcastSsdp(socket);
-    });
-  
-    socket.on('message', function (chunk, info) {  
-      console.log('[incoming] UDP message');
-      console.log(info);
-      console.log('MESSAGE', chunk.toString());
-    });
-  
-    console.log('binding to', c.ANY_IP + ':' + c.SSDP_SOURCE_PORT);
-    socket.bind(c.SSDP_SOURCE_PORT, c.ANY_IP);
+  socket.on('listening', function() {
+    console.log('socket ready...');
+    broadcastSsdp(socket);
+  });
 
-    setTimeout(() => {
-        socket.close();
-    }, 3000)
-  }
+  socket.on('message', function(chunk, info) {
+    console.log('[incoming] UDP message');
+    console.log(info);
+    console.log('MESSAGE', chunk.toString());
+  });
 
-//createSocket();
+  console.log('binding to', c.ANY_IP + ':' + c.SSDP_SOURCE_PORT);
+  socket.bind(c.SSDP_SOURCE_PORT, c.ANY_IP);
+
+  setTimeout(() => {
+    socket.close();
+  }, 3000);
+}
+
+createSocket();
